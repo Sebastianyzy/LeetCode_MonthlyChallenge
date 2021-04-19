e0967e114856aa8b4449750f3c3464700aafd512
Sebyzy <sebyzy@gmail.com>
2021-04-19 12:48:53 -0400

    4.19 update

--- Solution.java
+++ Solution.java
@@ -103,8 +103,6 @@ public class Solution {
 		// 1, 0, 0, 0 }, { 1, 1, 1, 1, 1 } };
 		// int[] result = kWeakestRows(mat, 3);
 		// printIntArray(result);
-
-
 	}
 
 	public static ListNode removeNthFromEnd(ListNode head, int n) {
