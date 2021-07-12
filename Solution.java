dbdb2184eeb7b2955ab30aa32e7cd927738f40a6
Sebyzy <sebyzy@gmail.com>
2021-07-12 09:48:09 -0400

    7.12 update

--- Solution.java
+++ Solution.java
@@ -105,6 +105,28 @@ public class Solution {
         // printIntArray(result);
     }
 
+    public static boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        HashMap<Character, Character> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char a = s.charAt(i);
+            char b = t.charAt(i);
+            if (map.containsKey(a)) {
+                if (!map.get(a).equals(b)) {
+                    return false;
+                }
+            } else {
+                if (map.containsValue(b)) {
+                    return false;
+                }
+                map.put(a, b);
+            }
+        }
+        return true;
+    }
+
     public static int kthSmallest(int[][] matrix, int k) {
         int[] ans = new int[matrix.length * matrix[0].length];
         int l = 0;
