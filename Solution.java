2310ebf16f7ccd44bced07df3aad55b107710e97
seb <sebyzy@gmail.com>
2021-08-02 10:22:30 -0400

    8.2 update

--- Solution.java
+++ Solution.java
@@ -107,6 +107,20 @@ public class Solution {
 
     }
 
+    public static int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        for(int i = 0; i<nums.length-1; i++ ){
+            for(int j = i+1; j<nums.length; j++){
+                if(nums[i]+nums[j] == target){
+                    ans[0] = i;
+                    ans[1] = j;
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }
+
     public static int[] getModifiedArray(int length, int[][] updates) {
         int[] ans = new int[length];
         for (int[] update : updates) {
