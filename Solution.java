5033a372de21a3424cd3ca821c9c27e12f77818e
seb <sebyzy@gmail.com>
2021-12-03 12:39:38 -0500

    12.3 update

--- Solution.java
+++ Solution.java
@@ -106,6 +106,18 @@ public class Solution {
         // printIntArray(result);
     }
 
+    public static int maxProduct(int[] nums) {
+        int ans = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            int temp = nums[i];
+            for (int j = i; j < nums.length; j++) {
+                temp *= j == i ? 1 : nums[j];
+                ans = Math.max(temp, ans);
+            }
+        }
+        return ans;
+    }
+
     public static int searchInsert(int[] nums, int target) {
         for(int i = 0; i < nums.length; i++){
             if(nums[i] >= target){
